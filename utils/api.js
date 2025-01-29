6d386bbcf94ddedc691119c0cb11ae5e352ef772
laruvas <bogdanovgleb938@gmail.com>
2025-01-30 01:05:00 +0300

    Editing

--- utils/api.js
+++ utils/api.js
@@ -1,12 +1,8 @@
 // Функция для запроса данных с API
 const API_URL = "https://jsonplaceholder.typicode.com/users";
 
-// 1. Верните вызов fetch, для доступа к методам Promise в основном файле скрипта
-// 2. Обработайте успешный ответ в методе then() -- верните данные в формате json
-// 3. Обработайте ошибку (если ответ не получен)
+
 // вот здесь можно найти пример обработки не успешного ответа -- https://habr.com/ru/articles/870774/
- 
-// Ваш код здесь...
 export function fetchUsers() {
  
   return fetch(API_URL)
